e8aae3ac849fc23b88dbc64b51c6b2ed410630b5
Coty Getzelman <coty-crg@live.com>
2016-12-11 00:42:37 -0800

    cleaned up the page

--- html/script.js
+++ html/script.js
@@ -25,10 +25,11 @@ function UpdateThreads(threads){
         $(".loading").remove();
         
         var threadurl = "https://boards.4chan.org/" + thread.board + "/thread/" + thread.no;  
-        var description = "<a href=\"" + threadurl + "\">/" + thread.board+ "/ - Name: " + thread.name + " Date: " + thread.now + "</a>";
-        var imghtml = "<img src=\"https://i.4cdn.org/" + thread.board +"/"+ thread.tim + "s" + thread.ext + "\"/>"; 
-        var html = "<div class=\"reply\"><span class=\"datetime\">" + description + "</span><br/>" + imghtml + "<blockquote class=\"postMessage\">" + thread.com + "</blockquote> </div>";
-        $(".threadlist").append( "<li class=\"threaditem\">" + html + "</li>");
+        var description = "<a href=\"" + threadurl + "\">/" + thread.board+ "/ - <span class=\"nameBlock\"><span class=\"name\">" + thread.name + "</span></span> " + thread.now + "</a>";
+        var imghtml = "<div class=\"file\"><a class=\"fileThumb\" href=\"#\" target=\"_blank\"><img src=\"https://t.4cdn.org/" + thread.board +"/"+ thread.tim + "s.jpg\" style=\"height: 125px; width: 125px;\" title /></a></div>"; 
+        
+        var html = "<div class=\"postContainer replyContainer\"><div class=\"post reply\"><span class=\"datetime\">" + description + "</span><br/>" + imghtml + "<blockquote class=\"postMessage\">" + thread.com + "</blockquote> </div></div>";
+        $(".threadlist").append( "<div class=\"threaditem\">" + html + "</div>");
     }); 
 }
 
