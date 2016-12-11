5f1e1ae40fb0ff74f2d2e4e89b186cf91dfc2cfc
Coty Getzelman <coty-crg@live.com>
2016-12-11 15:12:31 -0800

    update html

--- html/script.js
+++ html/script.js
@@ -26,7 +26,7 @@ function UpdateThreads(threads){
         
         var threadurl = "https://boards.4chan.org/" + thread.board + "/thread/" + thread.no;  
         var description = "<a href=\"" + threadurl + "\">/" + thread.board+ "/ - <span class=\"nameBlock\"><span class=\"name\">" + thread.name + "</span></span> " + thread.now + "</a>";
-        var imghtml = "<div class=\"file\"><a class=\"fileThumb\" href=\"#\" target=\"_blank\"><img src=\"https://t.4cdn.org/" + thread.board +"/"+ thread.tim + "s.jpg\" style=\"height: 125px; width: 125px;\" title /></a></div>"; 
+        var imghtml = "<div class=\"file\"><a class=\"fileThumb\" href=\"" + threadurl + "\" target=\"_blank\"><img src=\"https://t.4cdn.org/" + thread.board +"/"+ thread.tim + "s.jpg\" style=\"height: 125px; width: 125px;\" title /></a></div>"; 
         
         var html = "<div class=\"postContainer replyContainer\"><div class=\"post reply\"><span class=\"datetime\">" + description + "</span><br/>" + imghtml + "<blockquote class=\"postMessage\">" + thread.com + "</blockquote> </div></div>";
         $(".threadlist").append( "<div class=\"threaditem\">" + html + "</div>");
