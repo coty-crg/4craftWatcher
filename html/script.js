0940327741422a113a8eb1bc99d291953e53c88b
Coty Getzelman <coty-crg@live.com>
2016-12-12 23:41:13 -0800

    updated html

--- html/script.js
+++ html/script.js
@@ -25,7 +25,7 @@ function UpdateThreads(threads){
         $(".loading").remove();
         
         var threadurl = "https://boards.4chan.org/" + thread.board + "/thread/" + thread.no;  
-        var description = "<a href=\"" + threadurl + "\">/" + thread.board+ "/ - <span class=\"nameBlock\"><span class=\"name\">" + thread.name + "</span></span> " + thread.now + "</a>";
+        var description = "<a href=\"" + threadurl + "\"  target=\"blank\">/" + thread.board+ "/ - <span class=\"nameBlock\"><span class=\"name\">" + thread.name + "</span></span> " + thread.now + "</a>";
         var imghtml = "<div class=\"file\"><a class=\"fileThumb\" href=\"" + threadurl + "\" target=\"_blank\"><img src=\"https://t.4cdn.org/" + thread.board +"/"+ thread.tim + "s.jpg\" style=\"height: 125px; width: 125px;\" title /></a></div>"; 
         
         var html = "<div class=\"postContainer replyContainer\"><div class=\"post reply\"><span class=\"datetime\">" + description + "</span><br/>" + imghtml + "<blockquote class=\"postMessage\">" + thread.com + "</blockquote> </div></div>";
@@ -33,15 +33,35 @@ function UpdateThreads(threads){
     }); 
 }
 
-function Refresh(){
-    getData("https://archive.4craft.us/data", (data) => {
-        var foundThreads = []; 
-        var boards = data.boards;
-        var searchTerm = "anon"; 
+function UpdateArchivedThreads(threads){
+	    $(".archiveitem").remove(); 
+    
+    if(threads.length == 0){
+        $(".loadingarchive").replaceWith("<p class=\"nothreadsarchive\">No archived threads found.</p>");
+        return; 
+    }
+    
+    $(".nothreadsarchive").remove(); 
+    
+    threads.forEach(thread => {
+        $(".loadingarchive").remove();
         
-        console.log(data); 
+        var threadurl = "http://archived.moe/" + thread.board + "/thread/" + thread.no;  
+        var description = "<a href=\"" + threadurl + "\" target=\"blank\">/" + thread.board+ "/ - <span class=\"nameBlock\"><span class=\"name\">" + thread.name + "</span></span> "
+         + thread.now + "</a> [unique IPs: " + thread.unique_ips + ", replies: " + thread.replies + ", images: " + thread.images + "]";
+        var html = "<div class=\"postContainer replyContainer\"><div class=\"post reply\"><span class=\"datetime\">" + description + "</span><br/></div></div>";
+        $(".archivelist").append( "<div class=\"archiveitem\">" + html + "</div>");
+    }); 
+}
+
+function Refresh(){
+    getData("https://archive.4craft.us/data/live", (data) => {
         UpdateThreads(data); 
     }); 
+	
+	getData("https://archive.4craft.us/data/archive", (data) => {
+		UpdateArchivedThreads(data); 
+	});
 }
 
 Refresh(); 
